705daa776ea97f19617d37010503f31de9fda4b7
cany <couleurvide1008@gmail.com>
2023-06-28 12:05:26 +0900

    正面以外から見たときの動きをつける

--- src/App.js
+++ src/App.js
@@ -13,8 +13,115 @@ const imageArray = [
 ];
 
 function App() {
-  // 現在地の設定
   const [currentIndex, setCurrentIndex] = useState(0);
+  const ImgChange = () => {
+    switch (currentIndex) {
+      case 0:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(1)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(2)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(4)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(3)}>
+              ←
+            </button>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(0)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(2)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(5)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(3)}>
+              ←
+            </button>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(1)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(0)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(4)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(5)}>
+              ←
+            </button>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(1)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(5)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(4)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(0)}>
+              ←
+            </button>
+          </div>
+        );
+      case 4:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(0)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(2)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(5)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(3)}>
+              ←
+            </button>
+          </div>
+        );
+      case 5:
+        return (
+          <div className="buttons">
+            <button className="button-up" onClick={() => setCurrentIndex(1)}>
+              ↑
+            </button>
+            <button className="button-right" onClick={() => setCurrentIndex(2)}>
+              →
+            </button>
+            <button className="button-down" onClick={() => setCurrentIndex(4)}>
+              ↓
+            </button>
+            <button className="button-left" onClick={() => setCurrentIndex(3)}>
+              ←
+            </button>
+          </div>
+        );
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="App">
@@ -24,20 +131,7 @@ function App() {
       <div className="main">
         <img className="pawmi-picture" src={imageArray[currentIndex]} alt="" />
         {/* 上下左右の矢印を押したら画像が変わるようにする*/}
-        <div className="flex">
-          <button className="button-up" onClick={() => setCurrentIndex(1)}>
-            ↑
-          </button>
-          <button className="button-right" onClick={() => setCurrentIndex(2)}>
-            →
-          </button>
-          <button className="button-down" onClick={() => setCurrentIndex(4)}>
-            ↓
-          </button>
-          <button className="button-left" onClick={() => setCurrentIndex(3)}>
-            ←
-          </button>
-        </div>
+        <ImgChange />
       </div>
       <footer>
         <p>@2023 canymoff</p>
